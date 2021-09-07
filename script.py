42d9e75eee5523fe50d43cdc77a5d829b0bf8ce3
moh84shir <moh84shir@gmail.com>
2021-09-07 05:16:08 -0700

    bateer code.

--- script.py
+++ script.py
@@ -1,9 +1,9 @@
 import os
-RANGE_NUM = int(input('Number of dirs : '))
-NAME_RANGE = input('Name of dirs : ')
-DIR_NUM = RANGE_NUM + 1
-DIR_NAMES = list(range(0, DIR_NUM))
+RANGE_NUM = int(input('Count of dirs : '))
+NAME_DIR = input('Name of dirs : ')
+DIR_COUNT = RANGE_NUM + 1
+DIR_NAMES = list(range(0, DIR_COUNT))
 DIR_NAMES.pop(0)
 
 for DIR_NAME in DIR_NAMES:
-    os.system(f'mkdir {NAME_RANGE}_{DIR_NAME}')
+    os.system(f'mkdir {NAME_DIR}_{DIR_NAME}')
